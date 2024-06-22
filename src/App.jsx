15213e7d38c356af1acabeb565b2ad2fe0d5daf9
RickRo77 <rrickyroger@gmail.com>
2024-06-22 19:31:33 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // Define the API key and URL
+    const apiKey = '8PvjAV1hFU739FVOMhEAokrRbMxAGcPW'; // Replace 'YOUR_API_KEY' with your actual API key
+    const url1 = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    let url2='http://dataservice.accuweather.com/currentconditions/v1/'
+    let placeKey;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    // Create an instance of Axios with the API key
+    // Function to fetch weather data
+    const fetchPlaceData = async () => {
+      const axiosInstance = axios.create({
+        // headers: {
+        //   'apikey': apiKey,
+        // },
+        params: {
+          'apikey':apiKey,
+          'q':'Melbourne',
+        },
+      });
+      try {
+        const response = await axiosInstance.get(url1);
+        placeKey=(response.data[0].Key);
+        console.log(response);
+      } catch (error) {
+        console.log('eeerror');
+      }
+    };
+
+    const fetchWeatherData = async () => {
+      const axiosInstance2 = axios.create({
+        // headers: {
+        //   'apikey': apiKey,
+        // },
+        params: {
+          'apikey':apiKey,
+          'details':true,
+        },
+      });
+      try {
+        const response = await axiosInstance2.get(url2+placeKey);
+        // console.log(url2+placeKey);
+        console.log(response.data[0]);
+      } catch (error) {
+        console.log('eeerror');
+      }
+    };
+
+    // Call the function to fetch data
+    async function call (){
+    await fetchPlaceData();
+    await fetchWeatherData();
+    }
+
+    call();
+  }
 
 export default App
