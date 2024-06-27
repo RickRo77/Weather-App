196e6b3967ce467959815d4abe123fc29c6687b7
RickRo77 <rrickyroger@gmail.com>
2024-06-27 19:18:18 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: ['https:/https://rickro77.github.io/Weather-App/']
 })
